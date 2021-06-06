43463d76ba5d39f044e89d49680011d7a6713f79
Kfir Goldfarb <kggold4@gmail.com>
2021-06-06 23:23:22 +0300

    done!

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -11,258 +11,317 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
 #include <stack>
+#include <string>
+#include <list>
 
 using namespace std;
 
 namespace ariel {
 
-    // binary tree class
-    template<typename T>
+    template <class T>
     class BinaryTree {
+
     private:
 
-        // Node struct
-        struct Node {
+        struct Node
+        {
             T data;
             Node *parent, *left, *right;
 
             // default constructor
             Node(const T data, Node *parent = nullptr, Node *left = nullptr, Node *right = nullptr)
-                    : data(data), parent(parent), left(left), right(right) {}
-
-            // copy constructor
-            Node(Node &other) : data(other.data), parent(other.parent), left(other.left), right(other.right) {} {}
+                : data(data), parent(parent), left(left), right(right) {}
         };
 
-        // tree root
         Node *root;
 
         // inner private methods
-        void node_recursive_copy(Node *&other, Node *&original) {
+        // recursive copy of nodes
+        void node_recursive_copy(Node *&other, Node*&original) {
             if(other == nullptr) { original = nullptr; }
             else {
-                original = new Node(other);
-                copy(other->left, original->left);
-                copy(other->right, original->right);
+                original = new Node(other->data);
+                node_recursive_copy(other->left, original->left);
+                node_recursive_copy(other->right, original->right);
             }
         }
 
-        bool isEmpty() { return this->root == nullptr; }
+        // return true if root is null
+        bool empty() { return this->root == nullptr; }
+
+        // searching a node by data in recursive way
+        Node * find_node(Node * node, T data) {
+
+            // not found node
+            if(node == nullptr) { return nullptr; }
+
+            // found node
+            if(node->data == data) { return node;}
+
+            Node * left = nullptr;
+
+            // searching in left
+            if(node->left != nullptr) { left = find_node(node->left, data); }
+
+            Node * right = nullptr;
+
+            // searching in right
+            if(node->right != nullptr) { right = find_node(node->right, data); }
+
+            Node * ans = nullptr;
+
+            // find left
+            if(left != nullptr) { ans = left; }
+
+            // find right
+            else if(right != nullptr) { ans = right; }
+
+            // else
+            return ans;
+        }
+
+        // throw a message that cannot find node
+        void cannot_find_node() const { throw invalid_argument("cannot find node"); }
 
     public:
+
+        enum Order {PREORDER, INORDER, POSTORDER};
 
         // default constructor
         BinaryTree() { this->root = nullptr; }
 
         // copy constructor
-        BinaryTree(BinaryTree& binary_tree) {
-            if(isEmpty()) { this->root = nullptr; }
-            else { node_recursive_copy(binary_tree.root, this->root); }
+        BinaryTree(BinaryTree<T> &other) {
+            if(other.root == nullptr) { this->root = nullptr; }
+            else { node_recursive_copy(other.root, this->root); }
         }
 
         // de-constructor
-        ~BinaryTree() {}
+        ~BinaryTree() { if(!empty()) { delete root; } }
 
         // move constructor
-        BinaryTree(BinaryTree&& binaryTree) noexcept {
-            this->root = binaryTree.root;
+        BinaryTree(BinaryTree<T> &&other) noexcept {
+            this->root = other.root;
+            other.root = nullptr;
         }
 
         // move operator
-        BinaryTree &operator=(BinaryTree&& binaryTree) noexcept {
-            delete this->root;
-            this->root = binaryTree.root;
+        BinaryTree &operator=(BinaryTree<T> &&other) noexcept {
+            this->root = other.root;
+            other.root = nullptr;
         }
 
-        // regular = operator
-        BinaryTree &operator=(BinaryTree binaryTree) {
-            if(this == &binaryTree) {
-                return *this;
-            }
-            delete this->root;
-            root = new Node(binaryTree.root);
-            node_recursive_copy(binaryTree.root, this->root);
+        // simple = operator
+        BinaryTree &operator=(BinaryTree<T> other) {
+            if(this == &other) { return *this; }
+            this->root = new Node(other.root->data);
+            node_recursive_copy(other.root, this->root);
             return *this;
         }
 
-        // add root method
-        BinaryTree<T> &add_root(const T &data) {
+        BinaryTree &add_root(const T &data) {
 
-            // if current root is null
-            if(isEmpty()) { this->root = new Node(data); }
+            // if root is not define
+            if(empty()) { this->root = new Node(data); }
 
-            // replace current data of root
+            // change only his data
             else { this->root->data = data; }
+            return *this;
         }
 
-        // add left method
-        BinaryTree<T> &add_left(const T &parent_data, const T &child_data) {
-            if(isEmpty()) { throw(string) "root is null"; }
-            stack<Node*> st;
-            Node * current_node = this->root;
-            while(current_node != nullptr) {
-                st.push(current_node);
-                current_node = current_node->left;
-            }
-            while(!st.empty()) {
-                current_node = st.top();
-                st.pop();
-                if(current_node->data == parent_data) {
+        BinaryTree &add_left(const T &parent_data, const T &child_data) {
 
-                    // left node does not exist
-                    if(current_node->left == nullptr) { current_node->left = new Node(child_data); }
+            if(empty()) { cannot_find_node(); }
+            Node * node = find_node(this->root, parent_data);
 
-                    // change his data (exist)
-                    else { current_node->left->data = child_data; }
-                    return *this;
+            // cannot find the node
+            if(node == nullptr) { cannot_find_node(); }
+
+            // if left is null
+            if(node->left == nullptr) {
+                node->left = new Node(child_data);
+                node->left->parent = node;
+
+            // else
+            } else { node->left->data = child_data; }
+            return *this;
+        }
+
+        BinaryTree &add_right(const T &parent_data, const T &child_data) {
+            if(empty()) { cannot_find_node(); }
+            Node * node = find_node(this->root, parent_data);
+
+            // cannot find the node
+            if(node == nullptr) { cannot_find_node(); }
+
+            // if left is null
+            if(node->right == nullptr) {
+                node->right = new Node(child_data);
+                node->right->parent = node;
+
+                // else
+            } else { node->right->data = child_data; }
+            return *this;
+        }
+
+        class iterator {
+        private:
+
+            list<Node *> help_list;
+            Node *current_node;
+            stack<Node *> st;
+            Order order;
+
+        public:
+
+            // default constructor
+            iterator(const Order order = Order::INORDER, Node *node = nullptr) : current_node(node), order(order) {
+                if(this->current_node != nullptr) {
+
+                    //preorder
+                    if(this->order == Order::PREORDER) {
+
+                        // add right
+                        if(this->current_node->right != nullptr) { this->st.push(this->current_node->right); }
+
+                        // add left
+                        if(this->current_node->left != nullptr) { this->st.push(this->current_node->left); }
+                    }
+
+                    // inorder
+                    else if(this->order == Order::INORDER) {
+                        while(this->current_node != nullptr) {
+
+                            // add node
+                            this->st.push(this->current_node);
+
+                            // make node be his left
+                            this->current_node = this->current_node->left;
+                        }
+                        this->current_node = this->st.top();
+                        this->st.pop();
+                    }
+
+                    // postorder
+                    else {
+                        while(this->current_node != nullptr || !this->st.empty()) {
+
+                            // node exist
+                            if(this->current_node != nullptr) {
+                                this->st.push(this->current_node);
+                                this->current_node = this->current_node->left;
+
+                            // node is null
+                            } else {
+                                Node *node = this->st.top()->right;
+                                if(node == nullptr) {
+                                    node = this->st.top();
+                                    this->st.pop();
+                                    this->help_list.push_back(node);
+                                    while(!this->st.empty() && node == this->st.top()->right) {
+                                        node = this->st.top();
+                                        this->st.pop();
+                                        this->help_list.push_back(node);
+                                    }
+                                } else { this->current_node = node; }
+                            }
+                        }
+                        this->current_node = this->help_list.front();
+                        this->help_list.pop_front();
+                    }
+
                 }
-                current_node = current_node->right;
             }
-            throw invalid_argument("cannot found node");
-        }
 
+            T &operator*() const { return this->current_node->data; }
+            T *operator->() const { return &(this->current_node->data); }
 
-        // add right method
-        BinaryTree<T> &add_right(const T &parent_data, const T &child_data) {
-            if(isEmpty()) { throw(string) "root is null"; }
-            stack<Node*> st;
-            Node * current_node = this->root;
-            while(current_node != nullptr) {
-                st.push(current_node);
-                current_node = current_node->left;
-            }
-            while(!st.empty()) {
-                current_node = st.top();
-                st.pop();
-                if(current_node->data == parent_data) {
+            // operator ++
+            iterator &operator++() {
+                if(this->order == Order::PREORDER) {
+                    if(this->st.empty()) {
+                        this->current_node = nullptr;
+                        return *this;
+                    }
+                    this->current_node = this->st.top();
+                    this->st.pop();
 
-                    // right node does not exist
-                    if(current_node->right == nullptr) { current_node->right = new Node(child_data); }
+                    if(this->current_node->right != nullptr) { this->st.push(this->current_node->right); }
+                    if(this->current_node->left != nullptr) { this->st.push(this->current_node->left); }
+                } else if(this->order == Order::INORDER) {
+                    if(!this->st.empty() || this->current_node->right != nullptr) {
+                        this->current_node = this->current_node->right;
 
-                    // change his data (exist)
-                    else { current_node->left->data = child_data; }
-                    return *this;
+                        while(this->current_node != nullptr) {
+                            this->st.push(this->current_node);
+                            this->current_node = this->current_node->left;
+                        }
+
+                        this->current_node = this->st.top();
+                        this->st.pop();
+                    } else { this->current_node = nullptr; }
+                } else {
+                    if(!this->help_list.empty()) {
+                        this->current_node = this->help_list.front();
+                        this->help_list.pop_front();
+                    } else { this->current_node = nullptr; }
                 }
-                current_node = current_node->right;
+                return *this;
             }
-            throw invalid_argument("cannot found node");
+
+            // operator ++ int
+            iterator operator++(int) {
+                iterator iter = *this;
+                if(this->order == Order::PREORDER) {
+                    if(this->st.empty()) {
+                        this->current_node = nullptr;
+                        return iter;
+                    }
+                    this->current_node = this->st.top();
+                    this->st.pop();
+
+                    if(this->current_node->right != nullptr) { this->st.push(this->current_node->right); }
+                    if(this->current_node->left != nullptr) { this->st.push(this->current_node->left); }
+                } else if(this->order == Order::INORDER) {
+                    if(!this->st.empty() || this->current_node->right != nullptr) {
+                        this->current_node = this->current_node->right;
+
+                        while(this->current_node != nullptr) {
+                            this->st.push(this->current_node);
+                            this->current_node = this->current_node->left;
+                        }
+                        this->current_node = this->st.top();
+                        this->st.pop();
+                    } else { this->current_node = nullptr; }
+                } else {
+                    if(!this->help_list.empty()) {
+                        this->current_node = this->help_list.front();
+                        this->help_list.pop_front();
+                    } else { this->current_node = nullptr; }
+                }
+                return iter;
+            }
+
+            bool operator==(const iterator &other) const { return this->current_node == other.current_node; }
+            bool operator!=(const iterator &other) const { return this->current_node != other.current_node; }
+        };
+
+        iterator begin_preorder() { return iterator(Order::PREORDER, this->root); }
+        iterator end_preorder() { return iterator(Order::PREORDER, nullptr); }
+        iterator begin_inorder() { return iterator(Order::INORDER, this->root); }
+        iterator end_inorder() { return iterator(Order::INORDER, nullptr); }
+        iterator begin_postorder() { return iterator(Order::POSTORDER, this->root); }
+        iterator end_postorder() { return iterator(Order::POSTORDER, nullptr); }
+        iterator begin() { return iterator(Order::INORDER, this->root); }
+        iterator end() { return iterator(Order::INORDER, nullptr); }
+
+        friend ostream &operator<<(ostream &out, const BinaryTree &binary_tree) {
+            out << "binary tree" << endl;
+            return out;
         }
-
-        // output
-        // using other typename symbol
-        template<typename E>
-        friend ostream &operator<<(ostream &out, const BinaryTree<E> &binary_tree);
-
-        /**
-         * Nested Iterators Classes
-         */
-
-        // preorder iterator
-        class preorder_iterator {
-        private:
-            Node *current;
-        public:
-            preorder_iterator(Node *node = nullptr) : current(node) {}
-
-            T &operator*() const { return this->current->data; }
-
-            preorder_iterator &operator++() { return *this; }
-
-            preorder_iterator operator++(int value) { return *this; }
-
-            bool operator==(const preorder_iterator &it) const { return false; }
-
-            bool operator!=(const preorder_iterator &it) const { return false; }
-
-            preorder_iterator &operator=(const preorder_iterator &iterator) { return iterator; }
-
-            T *operator->() const { return &(this->current->data); }
-
-            int size() const { return 0; }
-        };
-
-        preorder_iterator begin_preorder() { return preorder_iterator(this->root); }
-
-        preorder_iterator end_preorder() { return preorder_iterator(); }
-
-        // inorder iterator
-        class inorder_iterator {
-        private:
-            Node *current;
-        public:
-            inorder_iterator(Node *node = nullptr) : current(node) {}
-
-            T &operator*() const { return this->current->data; }
-
-            inorder_iterator &operator++() { return *this; }
-
-            inorder_iterator operator++(int value) { return *this; }
-
-            bool operator==(const inorder_iterator &it) const { return false; }
-
-            bool operator!=(const inorder_iterator &it) const { return false; }
-
-            inorder_iterator &operator=(const inorder_iterator &iterator) { return iterator; }
-
-            T *operator->() const { return &(this->current->data); }
-
-            int size() const { return 0; }
-        };
-
-        inorder_iterator begin() { return begin_inorder(); }
-
-        inorder_iterator end() { return end_inorder(); }
-
-        inorder_iterator begin_inorder() { return inorder_iterator(this->root); }
-
-        inorder_iterator end_inorder() { return inorder_iterator(); }
-
-        // postorder iterator
-        class postorder_iterator {
-        private:
-            Node *current;
-        public:
-            postorder_iterator(Node *node = nullptr) : current(node) {}
-
-            T &operator*() const { return this->current->data; }
-
-            postorder_iterator &operator++() { return *this; }
-
-            postorder_iterator operator++(int value) { return *this; }
-
-            bool operator==(const postorder_iterator &it) const { return false; }
-
-            bool operator!=(const postorder_iterator &it) const { return false; }
-
-            postorder_iterator &operator=(const postorder_iterator &iterator) { return iterator; }
-
-            T *operator->() const { return &(this->current->data); }
-
-            int size() const { return 0; }
-        };
-
-        postorder_iterator begin_postorder() { return postorder_iterator(this->root); }
-
-        postorder_iterator end_postorder() { return postorder_iterator(); }
-
     };
-
-    /** Implementations */
-
-    template<typename T>
-    BinaryTree<T> &BinaryTree<T>::add_root(const T &data) { return *this; }
-
-    template<typename T>
-    BinaryTree<T> &BinaryTree<T>::add_left(const T &parent_data, const T &child_data) { return *this; }
-
-    template<typename T>
-    BinaryTree<T> &BinaryTree<T>::add_right(const T &parent_data, const T &child_data) { return *this; }
-
-    // implement out operator
-    template<typename T>
-    ostream &operator<<(ostream &out, const BinaryTree<T> &binary_tree) {
-        out << "output test";
-        return out;
-    }
 }
