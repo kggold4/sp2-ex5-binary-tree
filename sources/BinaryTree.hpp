bc7272de74d90371ce4c6427582ea749fd92144b
Kfir Goldfarb <kggold4@gmail.com>
2021-06-06 13:19:12 +0300

    trying to finish this fucking assignament

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
@@ -26,12 +27,28 @@ namespace ariel {
             T data;
             Node *parent, *left, *right;
 
+            // default constructor
             Node(const T data, Node *parent = nullptr, Node *left = nullptr, Node *right = nullptr)
                     : data(data), parent(parent), left(left), right(right) {}
+
+            // copy constructor
+            Node(Node &other) : data(other.data), parent(other.parent), left(other.left), right(other.right) {} {}
         };
 
         // tree root
         Node *root;
+
+        // inner private methods
+        void node_recursive_copy(Node *&other, Node *&original) {
+            if(other == nullptr) { original = nullptr; }
+            else {
+                original = new Node(other);
+                copy(other->left, original->left);
+                copy(other->right, original->right);
+            }
+        }
+
+        bool isEmpty() { return this->root == nullptr; }
 
     public:
 
@@ -39,19 +56,98 @@ namespace ariel {
         BinaryTree() { this->root = nullptr; }
 
         // copy constructor
-        BinaryTree(BinaryTree *binary_tree);
+        BinaryTree(BinaryTree& binary_tree) {
+            if(isEmpty()) { this->root = nullptr; }
+            else { node_recursive_copy(binary_tree.root, this->root); }
+        }
 
         // de-constructor
         ~BinaryTree() {}
 
+        // move constructor
+        BinaryTree(BinaryTree&& binaryTree) noexcept {
+            this->root = binaryTree.root;
+        }
+
+        // move operator
+        BinaryTree &operator=(BinaryTree&& binaryTree) noexcept {
+            delete this->root;
+            this->root = binaryTree.root;
+        }
+
+        // regular = operator
+        BinaryTree &operator=(BinaryTree binaryTree) {
+            if(this == &binaryTree) {
+                return *this;
+            }
+            delete this->root;
+            root = new Node(binaryTree.root);
+            node_recursive_copy(binaryTree.root, this->root);
+            return *this;
+        }
+
         // add root method
-        BinaryTree<T> &add_root(const T &data);
+        BinaryTree<T> &add_root(const T &data) {
+
+            // if current root is null
+            if(isEmpty()) { this->root = new Node(data); }
+
+            // replace current data of root
+            else { this->root->data = data; }
+        }
 
         // add left method
-        BinaryTree<T> &add_left(const T &parent_data, const T &child_data);
+        BinaryTree<T> &add_left(const T &parent_data, const T &child_data) {
+            if(isEmpty()) { throw(string) "root is null"; }
+            stack<Node*> st;
+            Node * current_node = this->root;
+            while(current_node != nullptr) {
+                st.push(current_node);
+                current_node = current_node->left;
+            }
+            while(!st.empty()) {
+                current_node = st.top();
+                st.pop();
+                if(current_node->data == parent_data) {
+
+                    // left node does not exist
+                    if(current_node->left == nullptr) { current_node->left = new Node(child_data); }
+
+                    // change his data (exist)
+                    else { current_node->left->data = child_data; }
+                    return *this;
+                }
+                current_node = current_node->right;
+            }
+            throw invalid_argument("cannot found node");
+        }
+
 
         // add right method
-        BinaryTree<T> &add_right(const T &parent_data, const T &child_data);
+        BinaryTree<T> &add_right(const T &parent_data, const T &child_data) {
+            if(isEmpty()) { throw(string) "root is null"; }
+            stack<Node*> st;
+            Node * current_node = this->root;
+            while(current_node != nullptr) {
+                st.push(current_node);
+                current_node = current_node->left;
+            }
+            while(!st.empty()) {
+                current_node = st.top();
+                st.pop();
+                if(current_node->data == parent_data) {
+
+                    // right node does not exist
+                    if(current_node->right == nullptr) { current_node->right = new Node(child_data); }
+
+                    // change his data (exist)
+                    else { current_node->left->data = child_data; }
+                    return *this;
+                }
+                current_node = current_node->right;
+            }
+            throw invalid_argument("cannot found node");
+        }
 
         // output
         // using other typename symbol
